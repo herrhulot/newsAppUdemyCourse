d7c159b4a969e0d357815407b0ec9aaf5db0bd38
herrhulot <petterwallgren83@gmail.com>
2020-10-07 17:14:15 +0200

    Lesson 259

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  runApp(News());
-}
-
-class News extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  runApp(App());
 }
